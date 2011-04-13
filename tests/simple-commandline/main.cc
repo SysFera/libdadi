41a8072305fb84926993c0c0e0759bfeebb50a08
Haikel Guemar <hguemar@sysfera.com>
2011-04-13 12:13:50 +0200

    flesh out sample: simple-comm

--- tests/simple-commandline/main.cc
+++ tests/simple-commandline/main.cc
@@ -1,5 +1,7 @@
 #include <cstdlib>
 #include <iostream>
+// force debug traces in dadi::setProperty
+#define NDEBUG
 
 #include "config.hh"
 #include "options.hh"
@@ -9,10 +11,17 @@ int main(int argc, char *argv[], char *envp[]) {
   dadi::Options opt;
   dadi::Config& config = dadi::Config::instance();
 
-  opt.setName("cline-1");
+  opt.setName("simple-commandline");
+  opt.setUsage("[-h] [options] -n <name> -j <number>");
   boost::function0<void> fHelp(boost::bind(dadi::help, boost::cref(opt)));
+  boost::function1<void, std::string> fName(
+    boost::bind(dadi::setPropertyString, "name", _1));
+  boost::function1<void, int> fJobs(
+    boost::bind(dadi::setProperty<int>, "jobs", _1));
 
   opt.addSwitch("help,h", "display help message", fHelp);
+  opt.addOption("name,n", "name", fName);
+  opt.addOption("jobs,j", "number of jobs", fJobs);
 
   opt.parseCommandLine(argc, argv);
   opt.notify();
