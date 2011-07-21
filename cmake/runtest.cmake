683c4f2e5e09c0b83c7f2920d424fb4baa35dbda
Eugene PAMBA CAPO-CHICHI <eugene.capochichi@sysfera.com>
2011-07-21 10:31:08 +0200

    added the macros for dadi tests

---
+++ cmake/runtest.cmake
@@ -0,0 +1,11 @@
+# wrapper allowing us to launch tests and redirect their output
+
+# don't use $ENV{XXX} as in documentation ==> does not work
+set( ENV{BOOST_TEST_CATCH_SYSTEM_ERRORS} "no" )
+execute_process( COMMAND ${BIN_PATH}/${TEST_PROG} 
+  --report_format=xml 
+  --report_level=detailed
+  --report_sink=${REPORT_PATH}/${TEST_PROG}.xml
+  OUTPUT_QUIET
+  ERROR_QUIET
+)
