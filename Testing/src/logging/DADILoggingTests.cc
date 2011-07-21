689def7799d255102cbd8e20c2a859536eac8a1b
Eugene PAMBA CAPO-CHICHI <eugene.capochichi@sysfera.com>
2011-07-21 17:00:42 +0200

    Added consoleChannel Test

--- Testing/src/logging/DADILoggingTests.cc
+++ Testing/src/logging/DADILoggingTests.cc
@@ -1,5 +1,5 @@
 #include <iostream>
-//#include <Logging/ConsoleChannel.hh>
+#include <Logging/ConsoleChannel.hh>
 #include <Logging/FileChannel.hh>
 //#include <Logging/LogServiceChannel.hh>
 #include <Logging/Logger.hh>
@@ -28,36 +28,48 @@ BOOST_AUTO_TEST_CASE( log_on_file_normal_call) {
   BOOST_TEST_MESSAGE("#FileChannel test normal call#");
   //The message which will be logged
   string msgToLog = "Dadi Logger tests: DEBUG";
-  try {
-    LoggerPtr mylogger1 = Logger::getLogger("test1");
-    //To check that mylogger1 is not NULL
-    BOOST_REQUIRE(mylogger1);
-    //To put the priority level
-    mylogger1->setLevel(Message::PRIO_DEBUG);
-    ChannelPtr cc1(new FileChannel);
-    cc1->putAttr<std::string>("path", temporaryFilename);
-    mylogger1->setChannel(cc1);
-    //To put the message to log
-    mylogger1->log(Message("", msgToLog, Message::PRIO_DEBUG));
-    //To check that the file created is not empty
-    BOOST_REQUIRE(((FileChannel* )cc1.get())->getSize() != 0);
-    bfs::path file (temporaryFilename);
-    //To check that the file with message logged is created
-    BOOST_REQUIRE(bfs::exists(file));
-    bfs::ifstream ifs (file);
-    // Read the whole file into string
-    std::stringstream ss;
-    ss << ifs.rdbuf();
-    //To check that the file contains the message to log
-    BOOST_REQUIRE(ss.str().find(msgToLog)!=string::npos);
-    //To remove the file created
-    boost::filesystem3::remove(file.filename());
-  } catch (...) {
-    BOOST_MESSAGE("FAILED\n");
-  }
+  LoggerPtr mylogger1 = Logger::getLogger("test1");
+  //To check that mylogger1 is not NULL
+  BOOST_REQUIRE(mylogger1);
+  //To put the priority level
+  mylogger1->setLevel(Message::PRIO_DEBUG);
+  ChannelPtr cc1(new FileChannel);
+  cc1->putAttr<std::string>("path", temporaryFilename);
+  mylogger1->setChannel(cc1);
+  //To put the message to log
+  mylogger1->log(Message("", msgToLog, Message::PRIO_DEBUG));
+  //To check that the file created is not empty
+  BOOST_REQUIRE(((FileChannel* )cc1.get())->getSize() != 0);
+  bfs::path file (temporaryFilename);
+  //To check that the file with message logged is created
+  BOOST_REQUIRE(bfs::exists(file));
+  bfs::fstream ifs (file);
+  std::string ss;
+  std::getline(ifs, ss);
+  //To check that the file contains the message to log
+  BOOST_REQUIRE(ss.compare(msgToLog) == 0);
+  //To remove the file created
+  boost::filesystem::remove(file.filename());
 }
 
+BOOST_AUTO_TEST_CASE( log_on_console_normal_call) {
 
+  BOOST_TEST_MESSAGE("#ConsoleChannel test normal call#");
+  //The message which will be logged
+  string msgToLog = "Dadi Logger tests: DEBUG";
+  stringstream oss;
+  LoggerPtr mylogger1 = Logger::getLogger("test1");
+  //To check that mylogger1 is not NULL
+  BOOST_REQUIRE(mylogger1);
+  //To put the priority level
+  mylogger1->setLevel(Message::PRIO_DEBUG);
+  ChannelPtr cc1(new ConsoleChannel(oss));
+  mylogger1->setChannel(cc1);
+  //To put the message to log
+  mylogger1->log(Message("", msgToLog, Message::PRIO_DEBUG));
+  //To check that the console content is the same that the message to log
+  BOOST_REQUIRE(oss.str().compare(msgToLog+"\n") == 0);
+}
 
 BOOST_AUTO_TEST_SUITE_END()
 
