0cc9efd170e1fdda9d301ad1883b73b931f8848f
Eugene PAMBA CAPO-CHICHI <eugene.capochichi@sysfera.com>
2011-07-25 14:52:59 +0200

    Added new tests on DADILoggingTests.cc

--- Testing/src/logging/DADILoggingTests.cc
+++ Testing/src/logging/DADILoggingTests.cc
@@ -30,12 +30,105 @@ static const string temporaryFilename = "crap4.log";
 
 BOOST_AUTO_TEST_SUITE (Dadi_test)
 
+
+BOOST_AUTO_TEST_CASE( initialize_logger_normal_call) {
+
+  BOOST_TEST_MESSAGE("#Initialize logger normal call#");
+  LoggerPtr mylogger1 = Logger::getLogger("initialize_logger");
+  //To check that mylogger1 is not NULL
+  BOOST_REQUIRE(mylogger1);
+}
+
+BOOST_AUTO_TEST_CASE( get_logger_name_normal_call) {
+
+  BOOST_TEST_MESSAGE("#Get logger name normal call#");
+  LoggerPtr mylogger1 = Logger::getLogger("logger_name");
+  //To check that mylogger1 is not NULL
+  BOOST_REQUIRE(mylogger1);
+  BOOST_REQUIRE(mylogger1->getName().compare("logger_name") == 0);
+}
+
+BOOST_AUTO_TEST_CASE( get_level_normal_call) {
+
+  BOOST_TEST_MESSAGE("#Get level normal call#");
+  LoggerPtr mylogger1 = Logger::getLogger("get_level_normal");
+  //To check that mylogger1 is not NULL
+  BOOST_REQUIRE(mylogger1);
+  //To put the priority level
+  mylogger1->setLevel(Message::PRIO_TRACE);
+  //To get the priority level
+  BOOST_REQUIRE(mylogger1->getLevel() == Message::PRIO_TRACE);
+  //To put the priority level
+  mylogger1->setLevel(Message::PRIO_DEBUG);
+  //To get the priority level
+  BOOST_REQUIRE(mylogger1->getLevel() == Message::PRIO_DEBUG);
+  //To put the priority level
+  mylogger1->setLevel(Message::PRIO_INFORMATION);
+  //To get the priority level
+  BOOST_REQUIRE(mylogger1->getLevel() == Message::PRIO_INFORMATION);
+  //To put the priority level
+  mylogger1->setLevel(Message::PRIO_WARNING);
+  //To get the priority level
+  BOOST_REQUIRE(mylogger1->getLevel() == Message::PRIO_WARNING);
+  //To put the priority level
+  mylogger1->setLevel(Message::PRIO_ERROR);
+  //To get the priority level
+  BOOST_REQUIRE(mylogger1->getLevel() == Message::PRIO_ERROR);
+  //To put the priority level
+  mylogger1->setLevel(Message::PRIO_FATAL);
+  //To get the priority level
+  BOOST_REQUIRE(mylogger1->getLevel() == Message::PRIO_FATAL);
+}
+
+BOOST_AUTO_TEST_CASE( get_level_bad_call) {
+
+  BOOST_TEST_MESSAGE("#Get level bad call (put nonexistent priority)#");
+
+  LoggerPtr mylogger1 = Logger::getLogger("get_level_bad");
+  //To check that mylogger1 is not NULL
+  BOOST_REQUIRE(mylogger1);
+  //To put a nonexistent level
+  mylogger1->setLevel(12);
+  //To get the priority level
+  BOOST_REQUIRE((mylogger1->getLevel() >= 1) && (mylogger1->getLevel() <= 7));
+}
+
+BOOST_AUTO_TEST_CASE( get_channel_normal_call) {
+
+  BOOST_TEST_MESSAGE("#Get channel normal call#");
+  stringstream oss;
+  LoggerPtr mylogger1 = Logger::getLogger("get_channel_normal");
+  //To check that mylogger1 is not NULL
+  BOOST_REQUIRE(mylogger1);
+  //To put the priority level
+  mylogger1->setLevel(Message::PRIO_DEBUG);
+  ChannelPtr cc1(new ConsoleChannel(oss));
+  BOOST_REQUIRE(cc1);
+  //To set a channel
+  mylogger1->setChannel(cc1);
+  //To get the channel
+  ChannelPtr cc2 = mylogger1->getChannel();
+  BOOST_REQUIRE(cc2);
+}
+
+BOOST_AUTO_TEST_CASE( get_channel_bad_call) {
+
+  BOOST_TEST_MESSAGE("#Get channel bad#");
+  stringstream oss;
+  LoggerPtr mylogger1 = Logger::getLogger("get_channel_bad");
+  //To check that mylogger1 is not NULL
+  BOOST_REQUIRE(mylogger1);
+  //To get the channel
+  ChannelPtr cc1 = mylogger1->getChannel();
+  BOOST_REQUIRE(!cc1);
+}
+
 BOOST_AUTO_TEST_CASE( log_on_file_normal_call) {
 
   BOOST_TEST_MESSAGE("#FileChannel test normal call#");
   //The message which will be logged
   string msgToLog = "Dadi Logger tests: DEBUG";
-  LoggerPtr mylogger1 = Logger::getLogger("test1");
+  LoggerPtr mylogger1 = Logger::getLogger("fileChannel_normal");
   //To check that mylogger1 is not NULL
   BOOST_REQUIRE(mylogger1);
   //To put the priority level
@@ -65,7 +158,7 @@ BOOST_AUTO_TEST_CASE( log_on_console_normal_call) {
   //The message which will be logged
   string msgToLog = "Dadi Logger tests: DEBUG";
   stringstream oss;
-  LoggerPtr mylogger1 = Logger::getLogger("test1");
+  LoggerPtr mylogger1 = Logger::getLogger("ConsoleChannel_normal");
   //To check that mylogger1 is not NULL
   BOOST_REQUIRE(mylogger1);
   //To put the priority level
