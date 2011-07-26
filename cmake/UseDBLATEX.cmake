6f948c346c6344a03320279a91d8375ffe9ded07
Haikel Guemar <haikel.guemar@sysfera.com>
2011-07-27 00:19:16 +0200

    fixup macros

--- cmake/UseDBLATEX.cmake
+++ cmake/UseDBLATEX.cmake
@@ -12,41 +12,41 @@ macro(dblatex_generate)
   set(options "")
   set(oneValueArgs STYLE OUTPUT TOPLEVEL_TARGET)
   set(multiValueArgs "")
-  cmake_parse_arguments(dblatex_generate
+  cmake_parse_arguments(dblatex_
     "${options}" "${oneValueArgs}" "${multiValueArgs}" ${ARGN})
   # we need at least that macros list docbook sources to be built
-  if(NOT dblatex_generate_UNPARSED_ARGUMENTS)
+  if(NOT dblatex__UNPARSED_ARGUMENTS)
     message(FATAL_ERROR
       "dblatex_generate can't generate document without any sources")
   endif()
-  if(NOT dblatex_generate_OUTPUT)
-    list(GET dblatex_generate_UNPARSED_ARGUMENTS 0 FIRST_ELT)
+  if(NOT dblatex_OUTPUT)
+    list(GET dblatex_UNPARSED_ARGUMENTS 0 FIRST_ELT)
     string(REGEX REPLACE
-      "(.*)\\.docbook" "\\1.pdf" dblatex_generate_OUTPUT ${FIRST_ELT})
+      "(.*)\\.docbook" "\\1.pdf" dblatex_OUTPUT ${FIRST_ELT})
   endif()
-  set(dblatex_generate_OUTPUT_OPT
-    "--output=${dblatex_generate_OUTPUT}")
-  if(dblatex_generate_STYLE)
-    set(dblatex_generate_STYLE
-      "--texstyle=${CMAKE_CURRENT_SOURCE_DIR}/${dblatex_generate_STYLE}")
+  set(dblatex_OUTPUT_OPT
+    "--output=${dblatex_OUTPUT}")
+  if(dblatex_STYLE)
+    set(dblatex_STYLE
+      "--texstyle=${CMAKE_CURRENT_SOURCE_DIR}/${dblatex_STYLE}")
   endif()
-  foreach(loop_var ${dblatex_generate_UNPARSED_ARGUMENTS})
-    set(dblatex_generate_UNPARSED_ARGUMENTS_OPT
-      "${dblatex_generate_UNPARSED_ARGUMENTS_OPT}"
+  foreach(loop_var ${dblatex_UNPARSED_ARGUMENTS})
+    set(dblatex_UNPARSED_ARGUMENTS_OPT
+      "${dblatex_UNPARSED_ARGUMENTS_OPT}"
       "${CMAKE_CURRENT_SOURCE_DIR}/${loop_var}")
   endforeach()
   
-  add_custom_target(${dblatex_generate_OUTPUT})
-  add_custom_command(TARGET ${dblatex_generate_OUTPUT} 
+  add_custom_target(${dblatex_OUTPUT})
+  add_custom_command(TARGET ${dblatex_OUTPUT} 
     COMMAND ${DBLATEX_EXECUTABLE}
-    ${dblatex_generate_STYLE}
-    ${dblatex_generate_OUTPUT_OPT}
-    ${dblatex_generate_UNPARSED_ARGUMENTS_OPT}
-    DEPENDS ${dblatex_generate_UNPARSED_ARGUMENTSZ}
+    ${dblatex_STYLE}
+    ${dblatex_OUTPUT_OPT}
+    ${dblatex_UNPARSED_ARGUMENTS_OPT}
+    DEPENDS ${dblatex_UNPARSED_ARGUMENTSZ}
     WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
     COMMENT "generate pdf from docbook sources")
-  if(dblatex_generate_TOPLEVEL_TARGET)
-    add_dependencies(${dblatex_generate_TOPLEVEL_TARGET}
-      ${dblatex_generate_OUTPUT})
+  if(dblatex_TOPLEVEL_TARGET)
+    add_dependencies(${dblatex_TOPLEVEL_TARGET}
+      ${dblatex_OUTPUT})
   endif()
 endmacro()
