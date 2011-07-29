3697ccb72c794d07a72c71db8cf53730c2f14d56
Haikel Guemar <haikel.guemar@sysfera.com>
2011-07-30 01:17:55 +0200

    Attributes tests: add some tests to check if getAttr/getAttrLists throws proper exceptions

--- Testing/src/attributes/DADIAttrTests.cc
+++ Testing/src/attributes/DADIAttrTests.cc
@@ -14,6 +14,7 @@
 #include "Config.hh"
 #include "Options.hh"
 #include "Attributes.hh"
+#include "Exception/Attributes.hh"
 
 
 using namespace std;
@@ -43,6 +44,38 @@ BOOST_AUTO_TEST_CASE(putAttr_ok) {
   BOOST_TEST_MESSAGE("# Get attribute normal call");
   Attributes attr;
   attr.putAttr<std::string>("/tmp", "toto");
+}
+
+BOOST_AUTO_TEST_CASE(getAttr_exception_unknown_attr) {
+  BOOST_TEST_MESSAGE("# Get Unknown attribute");
+  Attributes attr;
+  BOOST_REQUIRE_THROW(attr.getAttr<std::string>("toto"),
+                      UnknownAttributeError);
+}
+
+BOOST_AUTO_TEST_CASE(getAttr_exception_invalid_attr_1) {
+  BOOST_TEST_MESSAGE("# Get invalid attribute");
+  Attributes attr;
+  attr.putAttr<std::string>("toto", "toto");
+  // "toto" >> i (int) KO
+  BOOST_REQUIRE_THROW(attr.getAttr<int>("toto"),
+                      InvalidAttributeError);
+}
+
+BOOST_AUTO_TEST_CASE(getAttr_exception_invalid_attr_nothrow_1) {
+  BOOST_TEST_MESSAGE("# Get invalid attribute");
+  Attributes attr;
+  attr.putAttr<std::string>("toto", "1");
+  // "1" >> i (int) OK
+  BOOST_REQUIRE_NO_THROW(attr.getAttr<int>("toto"));
+}
+
+BOOST_AUTO_TEST_CASE(getAttr_exception_invalid_attr_nothrow_2) {
+  BOOST_TEST_MESSAGE("# Get invalid attribute");
+  Attributes attr;
+  attr.putAttr<int>("toto", 1);
+  // 1 >> s (string) OK
+  BOOST_REQUIRE_NO_THROW(attr.getAttr<std::string>("toto"));
 }
 
 BOOST_AUTO_TEST_SUITE_END()
