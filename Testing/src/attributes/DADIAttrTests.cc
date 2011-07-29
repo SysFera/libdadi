b708f6480cf5e979078efb97b4b398e2a05e5416
Haikel Guemar <haikel.guemar@sysfera.com>
2011-07-30 01:10:26 +0200

    Attributes tests: simplified code

--- Testing/src/attributes/DADIAttrTests.cc
+++ Testing/src/attributes/DADIAttrTests.cc
@@ -9,41 +9,40 @@
 #include <iostream>
 #include <sstream>
 
+#include <boost/test/unit_test.hpp>
+
 #include "Config.hh"
 #include "Options.hh"
 #include "Attributes.hh"
 
-#include <boost/test/unit_test.hpp>
 
 using namespace std;
 using namespace dadi;
 
-
 BOOST_AUTO_TEST_SUITE(Dadi_test_attr)
-
 
 BOOST_AUTO_TEST_CASE(getAttr_ok) {
   BOOST_TEST_MESSAGE("# Get attribute normal call");
   Attributes attr;
-  string s = "";
+  string s;
   attr.putAttr<string>("/tmp", "toto");
-  s=attr.getAttr<string>("/tmp", "");
-  BOOST_REQUIRE(s.compare("toto")==0);
+  s = attr.getAttr<string>("/tmp", "");
+  BOOST_REQUIRE("toto" == s);
 }
 
 BOOST_AUTO_TEST_CASE(getAttrNoParam_ok) {
   BOOST_TEST_MESSAGE("# Get attribute normal call no value");
   Attributes attr;
-  string s = "";
-  s=attr.getAttr<string>("/tmp", "toto");
-  BOOST_REQUIRE(s.compare("toto")==0);
+  string s;
+  s = attr.getAttr<string>("/tmp", "toto");
+  BOOST_REQUIRE("toto" == s);
 }
 
 // TODO: Pertinence de ce test ?
 BOOST_AUTO_TEST_CASE(putAttr_ok) {
   BOOST_TEST_MESSAGE("# Get attribute normal call");
-  Attributes attrib ;
-  attrib.putAttr<std::string>("/tmp", "toto");
+  Attributes attr;
+  attr.putAttr<std::string>("/tmp", "toto");
 }
 
 BOOST_AUTO_TEST_SUITE_END()
