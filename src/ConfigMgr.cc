ebc4a08b8eeca7a5668df3b2f53de160fc403246
Benjamin Depardon <benjamin.depardon@sysfera.com>
2011-09-29 14:54:43 +0200

    Fixed a few fix me, and cpplint errors

---
+++ src/ConfigMgr.cc
@@ -0,0 +1,33 @@
+/**
+ * @file   ConfigMgrMgr.hh
+ * @author Benjamin Depardon <benjamin.depardon@sysfera.com>
+ * @brief  local configuration manager implementation
+ * @section Licence
+ *  |LICENCE|
+ *
+ */
+
+
+#include "ConfigMgr.hh"
+
+namespace dadi {
+
+ConfigMgr::ConfigMgr(dadi::ConfigStore& store)
+  : proxyCache(new ProxyCache(store)), rootKey("undefined") {}
+
+
+void
+ConfigMgr::setRootKey(const std::string& key) {
+  rootKey = key;
+}
+
+std::string
+ConfigMgr::getRootKey() const {
+  return rootKey;
+}
+
+
+ConfigMgr::ProxyCache::ProxyCache(const dadi::ConfigStore& store)
+  : store_(store) {}
+
+} /* namespace dadi */
