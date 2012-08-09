1527a636197ca33583e27d19f0e5bfb80df9dd68
Benjamin Depardon <benjamin.depardon@sysfera.com>
2012-08-09 10:30:25 +0200

    Display DADI_INSTALL_DIR

--- utils/FindDadi.cmake
+++ utils/FindDadi.cmake
@@ -13,6 +13,19 @@ if(NOT WIN32)
   # returns DADI_INCLUDE_DIRS and DADI_LIBRARY_DIRS
 endif()
 
+set(DADI_INSTALL_DIR_STRING "directory containing the file share/cmake/Modules/FindDadi.cmake. This is either the root of the build tree, or PREFIX of an installation.")
+
+find_path(DADI_INSTALL_DIR
+  # Look for this file
+  NAMES share/cmake/Modules/FindDadi.cmake
+  
+  # Descend one level, if necessary
+  PATH_SUFFIXES ..
+  
+  # Help the user find it if we cannot.
+  DOC "The ${DADI_INSTALL_DIR_STRING}"
+  )
+
 find_path(DADI_INCLUDE_DIR
   NAMES dadi/dadi.hh
   PATHS ${DADI_INSTALL_DIR}/include
